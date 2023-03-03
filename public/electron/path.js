2148fa2955e9906ad71c98b3877cf9f8f12963ab
shyunku <whdudgns7321@gmail.com>
2023-03-03 16:05:04 +0900

    fix win installer issue

--- public/electron/path.js
+++ public/electron/path.js
@@ -1,28 +1,28 @@
-const {app} = require('electron');
-const path = require('path');
-const package = require('../../package.json');
+const { app } = require("electron");
+const path = require("path");
+const package = require("../../package.json");
 
-const appDataPath = app.getPath('userData');
+const appDataPath = app.getPath("userData");
 const appPath = app.getAppPath();
 const isBuildMode = !process.env.ELECTRON_START_URL;
-const datafilesDirname = 'datafiles';
+const datafilesDirname = "datafiles";
 
 let schemeVersion = package.schemeVersion;
-const rootDir = isBuildMode ? appDataPath : path.join(__dirname, '../../');
+const rootDir = isBuildMode ? appDataPath : path.join(__dirname, "../../");
 
 module.exports = {
-    directory: {
-        root: rootDir,
-        core: path.join(rootDir, datafilesDirname, schemeVersion, 'core'),
-        userAccountDatabase: path.join(rootDir, datafilesDirname, schemeVersion, 'databases'),
-    },
-    path: {
-        coreDB: path.join(rootDir, datafilesDirname, schemeVersion, 'core', 'core.sqlite3'),
-        coreTemplateDB: isBuildMode ?
-            path.join(appPath, 'build', 'default', 'core-template.sqlite3') :
-            path.join(rootDir, 'public', 'default', 'core-template.sqlite3'),
-        userTemplateDB: isBuildMode ?
-            path.join(appPath, 'build', 'default', 'user-template.sqlite3') :
-            path.join(rootDir, 'public', 'default', 'user-template.sqlite3'),
-    }
+  directory: {
+    root: rootDir,
+    core: path.join(rootDir, datafilesDirname, schemeVersion, "core"),
+    userAccountDatabase: path.join(rootDir, datafilesDirname, schemeVersion, "databases"),
+  },
+  path: {
+    coreDB: path.join(rootDir, datafilesDirname, schemeVersion, "core", "core.sqlite3"),
+    coreTemplateDB: isBuildMode
+      ? path.join(appPath, "../", "default", "core-template.sqlite3")
+      : path.join(rootDir, "public", "default", "core-template.sqlite3"),
+    userTemplateDB: isBuildMode
+      ? path.join(appPath, "../", "default", "user-template.sqlite3")
+      : path.join(rootDir, "public", "default", "user-template.sqlite3"),
+  },
 };
